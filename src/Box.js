00424799329e1308ab696850da70c9f1425c20e7
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-06-15 12:28:21 -0700

    Add styling to boxes

--- src/Box.js
+++ src/Box.js
@@ -3,20 +3,17 @@ import './Box.css'
 import {choice} from './helpers'
 
 class Box extends Component{
-    static defaultProps = {
-        allColors: ["#001f3f", "#01FF70", "#F012BE", "#ffbbee", "#fe8a71", "#e7d3d3", "#ff8b94", "#ffd3b6",
-    "#B10DC9","#6497b1" , "#8874a3",  "#f4b6c2","#f6abb6", "#fe4a49", "#2ab7ca" , "#84c1ff"] 
-    }
+
     constructor(props) {
         super(props)
-        this.state = { color:choice(this.props.allColors) }
+        this.state = { color:choice(this.props.color) }
         this.handleClick = this.handleClick.bind(this)
     }
 
     pickColor() {
         let newColor
         do {
-            newColor = choice(this.props.allColors)
+            newColor = choice(this.props.color)
         } while (newColor === this.state.color)
         
         this.setState({color: newColor})
--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import Box from './Box'
+import './BoxContainer.css'
 
 class BoxContainer extends Component{
     static defaultProps = {
-        maxBoxes: 18
+        maxBoxes: 18,
+        allColors: ["#001f3f", "#01FF70", "#F012BE", "#ffbbee", "#fe8a71", "#e7d3d3", "#ff8b94", "#ffd3b6",
+    "#B10DC9","#6497b1" , "#8874a3",  "#f4b6c2","#f6abb6", "#fe4a49", "#2ab7ca" , "#84c1ff"] 
     }
     render() {
         const boxes = Array.from({ length: this.props.maxBoxes })
-            .map(() => <Box />)
+            .map(() => <Box color={this.props.allColors}/>)
         
             return <div className="BoxContainer">{boxes}</div>
     }
